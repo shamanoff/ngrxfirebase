9870f0fceed7c55d3daa2a840ad2f742486f24df
Shamanoff <shamanoff@gmail.com>
2017-12-25 16:34:35 +0200

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+
+
+interface AppState {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  message$: Observable<string>;
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'});
+  }
+
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'});
+  }
 }
